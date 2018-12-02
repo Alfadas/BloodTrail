e829b4e5aaa7275b328f2991da8033490a718fe3
kruemelkeksfan <a_maximilian@web.de>
2018-12-02 19:02:45 +0100

    Added kruemelkeksfans test scene, CharacterManager.cs and started building a UI

---
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+	{
+	[SerializeField] int maxcharacters = 6;
+
+	private Character[] characters;
+
+	void Start()
+		{
+		characters = new Character[maxcharacters];
+		for(int I = 0; I < characters.Length; ++I)
+			{
+			characters[I] = null; // TODO: Instantiate new character here
+			}
+		}
+
+	void Update()
+		{
+
+		}
+	}
