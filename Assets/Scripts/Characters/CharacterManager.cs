14d49def42fe6f26aa8d0d7cde45393633557267
kruemelkeksfan <a_maximilian@web.de>
2018-12-03 06:34:32 +0100

    Added random character names and (unclosable + infinitely stacking) UI-buttons for each character

--- Assets/Scripts/Characters/CharacterManager.cs
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterManager : MonoBehaviour
 	{
@@ -8,16 +9,20 @@ public class CharacterManager : MonoBehaviour
 	[SerializeField] int maxcombatcharacters = 5;
 	[SerializeField] int startcharacters = 5;
 	[SerializeField] GameObject[] characterprefabs = new GameObject [3];
+	[SerializeField] int maleprefabcount = 2;
+	[SerializeField] Button parentbutton;
+	[SerializeField] Button characterbutton;
+	[SerializeField] Button closebutton;
 
 	private List<GameObject> characters;
+	private List<Button> characterbuttons;
 
 	void Start()
 		{
-		System.Random random = new System.Random();
 		characters = new List<GameObject>(maxcharacters);
 		for(int I = 0; I < startcharacters; ++I)
 			{
-			characters.Add(Instantiate(characterprefabs[random.Next(characterprefabs.Length)])); // TODO: New characters spawn at (0, 0, 0), would there be a better place?
+			addCharacter();
 			}
 		}
 
@@ -27,5 +32,47 @@ public class CharacterManager : MonoBehaviour
 			{
 			character.GetComponent<Character>().updateCharacter(time);
 			}
+		}
+
+	public void generateCharacterSelection()
+		{
+		int x = 80;
+		int y = 60;
+		Button button;
+
+		foreach(GameObject character in characters)
+			{
+			button = Instantiate(characterbutton, new Vector3(x, y, 0), Quaternion.identity);
+			button.gameObject.transform.SetParent(parentbutton.gameObject.transform, false);
+			button.GetComponentInChildren<Text>().text = character.GetComponent<Character>().getName();
+			y += 40;
+			}
+
+		button = Instantiate(closebutton, new Vector3(x, y, 0), Quaternion.identity);
+		button.gameObject.transform.SetParent(parentbutton.gameObject.transform, false);
+		}
+
+	public void addCharacter()
+		{
+		System.Random random = new System.Random(); // TODO: seed?
+		int prefabindex = random.Next(characterprefabs.Length);
+		GameObject character = Instantiate(characterprefabs[prefabindex]);  // TODO: New characters spawn at (0, 0, 0), would there be a better place?
+
+		characters.Add(character);
+		if(prefabindex < maleprefabcount)
+			{
+			character.GetComponent<Character>().rollName(true);
+			}
+		else
+			{
+			character.GetComponent<Character>().rollName(false);
+			}
+		character.GetComponent<Character>().setManager(this);
+		}
+
+	public void killCharacter(GameObject character)
+		{
+		characters.Remove(character);
+		Destroy(character);
 		}
 	}
